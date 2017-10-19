71c92478173827e97a13a7b8e15d1c2cd1291492
ascrutae <nzt48.xin@gmail.com>
2017-10-19 23:42:04 +0800

    suit new version of skywalking

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-echo "replace {COLLECTOR_SERVER} to $COLLECTOR_SERVER"
-eval sed -i -e 's/\{COLLECTOR_SERVER\}/$COLLECTOR_SERVER/' /usr/local/tomcat/agent-config/sky-walking.config
 exec "$@"
